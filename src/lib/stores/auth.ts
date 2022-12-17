b276950987971534719b9a86b4aa8990c7563efa
Jeroen Pelgrims <jeroen.pelgrims@gmail.com>
2022-12-17 19:40:35 +0100

    Fix issue where the initial SSR would return a page where the user data is undefined. (It still does that, but we do an immediate location.reload() to fix the page.

--- src/lib/stores/auth.ts
+++ src/lib/stores/auth.ts
@@ -6,20 +6,37 @@ import { auth } from '../firebase/client';
 
 export const user = writable<User | null>(null);
 
-export function signOut() {
-	auth.signOut();
+export async function signOut() {
+	return auth.signOut();
 }
 
 export async function signIn() {
-	return signInWithRedirect(auth, new GoogleAuthProvider());
+	await signInWithRedirect(auth, new GoogleAuthProvider());
 }
 
 if (browser) {
 	auth.onIdTokenChanged(async (newUser) => {
+		const tokenCurrentlySet = cookie.parse(document.cookie)['token'] !== undefined;
+		const token = newUser ? await newUser?.getIdToken() : undefined;
+		document.cookie = cookie.serialize('token', token ?? '', {
+			path: '/',
+			maxAge: token ? undefined : 0
+		});
 		user.set(newUser);
 
-		const token = newUser ? await newUser?.getIdToken() : undefined;
-		const tokenCookie = cookie.serialize('token', token ?? '', { path: '/' });
-		document.cookie = tokenCookie;
+		/* WHY:
+			Explained in the bottom of this section in my blogpost:
+			https://jeroenpelgrims.com/access-the-firebase-auth-user-in-sveltekit-server-side/#3-create-auth-store-set-id-token-cookie
+		*/
+		if (!tokenCurrentlySet && token) {
+			document.location.reload();
+		}
 	});
+
+	// refresh the ID token every 10 minutes
+	setInterval(async () => {
+		if (auth.currentUser) {
+			await auth.currentUser.getIdToken(true);
+		}
+	}, 10 * 60 * 1000);
 }
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,11 +1,18 @@
 import { auth } from '$lib/firebase/admin';
+import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoadEvent } from './$types';
 
 export async function load({ cookies }: LayoutServerLoadEvent) {
-	const token = cookies.get('token');
-	const user = token ? await auth.verifyIdToken(token) : null;
-
-	return {
-		uid: user?.uid
-	};
+	try {
+		const token = cookies.get('token');
+		const user = token ? await auth.verifyIdToken(token) : null;
+		return {
+			uid: user?.uid
+		};
+	} catch (err) {
+		// token not set, not valid or expired
+		// unset cookie and redirect to main page
+		cookies.set('token', '', { maxAge: -1 });
+		redirect(307, '/');
+	}
 }
